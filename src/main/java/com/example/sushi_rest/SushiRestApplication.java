80f194c58648194ab308f0cf26f4d9f327025e59
CortivoGiacomo <giacomocortivo1@gmail.com>
2024-02-09 11:55:47 +0100

    Maniglio Bot

---
+++ src/main/java/com/example/sushi_rest/SushiRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.sushi_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SushiRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SushiRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sushi_rest/SushiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sushi_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SushiRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
